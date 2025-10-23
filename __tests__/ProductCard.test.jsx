d2f92f836414cb0760e2af54b4968f9467c216ad
Andres Torres <andr.torres@duocuc.cl>
2025-10-22 22:16:36 -0300

    Version arreglada de pruebas unitarias

--- __tests__/ProductCard.test.jsx
+++ __tests__/ProductCard.test.jsx
@@ -1,15 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import ProductCard from "../components/ProductCard";
+import ProductCard from "../src/components/molecules/ProductCard";  
 
 const product = { name: "Torta de Chocolate", price: 45000 };
 
 test("ProductCard muestra nombre y precio", () => {
   render(<ProductCard product={product} onAdd={() => {}} />);
-
   expect(screen.getByText("Torta de Chocolate")).toBeInTheDocument();
-
-
-  const priceElement = screen.getByText("$45.000");
-  expect(priceElement).toBeInTheDocument();
-
+  expect(screen.getByText("$45.000")).toBeInTheDocument();
 });
