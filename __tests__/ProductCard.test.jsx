c1d79aff7df69fc3caf04a3468e71887af6fc92f
Andres Torres <andr.torres@duocuc.cl>
2025-10-22 21:56:09 -0300

    Solucion e pruebas unitarias

--- __tests__/ProductCard.test.jsx
+++ __tests__/ProductCard.test.jsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
-import ProductCard from "@/components/molecules/ProductCard";
+import ProductCard from "../components/ProductCard";
 
 const product = { name: "Torta de Chocolate", price: 45000 };
 
 test("ProductCard muestra nombre y precio", () => {
   render(<ProductCard product={product} onAdd={() => {}} />);
+
   expect(screen.getByText("Torta de Chocolate")).toBeInTheDocument();
-  expect(screen.getByText(/\$45,000/i)).toBeInTheDocument();
+
+
+  const priceElement = screen.getByText("$45.000");
+  expect(priceElement).toBeInTheDocument();
+
 });
--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,12 @@ export default {
   moduleFileExtensions: ["js", "jsx"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    "\\.(css|less|scss)$": "identity-obj-proxy",
-    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/src/__mocks__/fileMock.js"
+    "\\.(css|less|scss)$": "identity-obj-proxy", 
+    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/src/__mocks__/fileMock.js", 
+    "^@/(.*)$": "<rootDir>/src/$1", 
   },
   testMatch: [
     "**/src/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[jt]s?(x)"
+    "**/?(*.)+(spec|test).[jt]s?(x)",
   ],
 };
